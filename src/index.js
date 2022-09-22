d44c269d2bdfa0d0b52974969a3890ee49703826
Hanna <hannamlittlefield@gmail.com>
2022-09-22 13:26:05 -0400

    setting up express and mongoose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+const express = require('express')
+const app = express()
 var http = require("http");
 const mongoose = require('mongoose');
+const port = process.env.PORT || 5000;
+
 
 //connect to mongo
 mongoose.connect('mongodb+srv://greenfielddev:Sh3rl0ck@cluster0.zlfjykj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+//test to see if server is running
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //create a server object:
 http

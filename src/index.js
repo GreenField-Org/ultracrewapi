1f1b676df2dfa9cd5969f59712dae5892e888105
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-10-29 16:09:20 -0400

    8 get user (#22)
    * got POST working, starting GET for user
    
    * cleaning up the api
    
    * cleanup
    
    * fixing exposed creds to db
    
    * errors on post api
    
    * adding a check in the post user to verify existence of doc user get working

--- src/index.js
+++ src/index.js
@@ -1,29 +1,36 @@
+let dotenv = require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
 const app = express()
 app.use(helmet())
-var http = require("http");
+const http = require("http");
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
 
+var port = process.env.PORT || 5000
+var jsonParser = bodyParser.json()
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //connect to mongo
-mongoose.connect('mongodb+srv://greenfielddev:Sh3rl0ck@cluster0.zlfjykj.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 //test to see if server is running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
 //create a server object:
 http
   .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
-    res.end(); //end the response
+    res.write("Hello World!") //write a response to the client
+    res.end() //end the response
   })
-  .listen(8080); //the server object listens on port 8080
+  .listen(8080) //the server object listens on port 8080
 
 
  //creating a user
  const Schema = mongoose.Schema
+
  const userSchema = new Schema({
   username: String,
   firstName: String,
@@ -37,45 +44,52 @@ http
   email: String,
   phoneNum: String,
   userTier: Number
-})
+}, {timestamps: true})
+
 const User = mongoose.model("User", userSchema)
-  app.post('/api/user', (req, res) =>{
-  //assign body item w username in it to username var
-  const username = req.body.username
-  const firstName = req.body.firstName
-  const lastName = req.body.lastName
-  const password = req.body.password
-  const middleInitial = req.body.middleInitial
-  const dob = req.body.dob
-  const weight = req.body.weight
-  const country = req.body.country
-  const address = req.body.address
-  const email = req.body.email
-  const phoneNum = req.body.phoneNum
-  const userTier = req.body.userTier
- //check if username is empty
- if (username === ''){
-   res.json('Username required')
- }else
- //create new username obj in the Users collection
- // the var id turns into the _id from the result of the created username
-  User.create({
-    username: username,
-    firstName: firstName,
-    lastName: lastName,
-    middleInitial: middleInitial, 
-    password: password,
-    dob: dob,
-    weight: weight,
-    country: country,
-    address: address,
-    email: email,
-    phoneNum: phoneNum,
-    userTier: userTier
-  }).then(result => {
-    id = result._id
-    res.json(result)
+
+app.get('/api/user/:id', urlencodedParser, (req, res) => {
+  User.findById(req.params.id, (err, docs) => {
+    if (err) {
+      res.sendStatus(404)
+    } else {
+      res.status(200).json({ user: docs, status: 200})
+    }
   })
+});
+
+app.post('/api/user', jsonParser, (req, res) => {
+    const user = {
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      middleInitial: req.body.middleInitial,
+      password: req.body.password,
+      dob: req.body.dob,
+      weight: req.body.weight,
+      country: req.body.country,
+      address: req.body.address,
+      email: req.body.email,
+      phoneNum: req.body.phoneNum,
+      userTier: req.body.userTier
+    }
+
+  //check if username is empty
+  if (user.username === '') {
+    res.status(500).json('Username required')
+  } else {
+    //create new username obj in the Users collection
+    // the var id turns into the _id from the result of the created username
+    if (User.find({username: req.body.username}).count() > 0) {
+      res.status(500).json({message: 'EXISTING_USER'})
+    } else {
+      User.create(user).then(result => {
+        id = result._id
+        res.status(200).json(result)
+      })
+    }
+  
+  }
 })
 
 
